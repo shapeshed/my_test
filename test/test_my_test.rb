9c4b38b6ad830ca1dfb27235942027bd7545d3e6
George Ornbo <george@shapeshed.com>
2009-11-11 16:39:35 +0000

    Initial commit to my_test.

---
+++ test/test_my_test.rb
@@ -0,0 +1,7 @@
+require 'helper'
+
+class TestMyTest < Test::Unit::TestCase
+  should "probably rename this file and start testing for real" do
+    flunk "hey buddy, you should probably rename this file and start testing for real"
+  end
+end
